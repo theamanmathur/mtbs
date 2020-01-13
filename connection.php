63b5f6010871384f118a6ce9924f5b6b990d63cf
Aman Mathur <25167612+wolfoxx@users.noreply.github.com>
2020-01-13 14:52:44 +0530

    credentials modified

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php  global $conn;
-        $servername = "sql312.epizy.com";  
-        $username = "epiz_25031589";  
-        $password = "CNqe7A99hQD6w";  
-        $db='epiz_25031589_mtbs';
+        $servername = "*************";
+        $username = "*************";
+        $password = "*************";
+        $db="*************";
         $conn = mysqli_connect($servername,$username, $password, $db);
         if ( $conn->connect_error) {
             die("Connection failed: " .$conn->connect_error);
